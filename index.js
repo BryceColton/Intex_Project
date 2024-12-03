35e81f17766a2e5930374e284169166b6c096523
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-02 17:57:24 -0700

    finished merge

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
 const express = require("express");
-// This imports the express library
-
 const app = express();
-// this calls the express objects gaining the attributes and values
-
 const path = require("path");
-// This is the path class allowing access to directory
+const dotenv = require('dotenv');
 
-const dotenv = require('dotenv')
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Load environment variables from .env file
 dotenv.config();
 
-
 const port = process.env.PORT || 3000;
-// Assigning the port number to 5000
 
+// Set view engine to ejs
 app.set("view engine", "ejs");
-// This sets the view engine to ejs instead of html
 
+// Set views directory
 app.set("views", path.join(__dirname, "views"));
-// this calls the views folder and points to the directory and all of its files
 
+// Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public")); //give access to the public folder.
-
+// Initialize knex with database connection settings
 const knex = require("knex")({
-  client: "pg",
-  connection: {
-    host: process.env.RDS_HOST,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DATABASE,
-    port: process.env.RDS_PORT,
-    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
-  },
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DATABASE,
+        port: process.env.RDS_PORT,
+        ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+    }
 });
-// This is all of the information needed to access postgres
-
-// This is the get method for the root file
 
 app.get('/', (req, res) => {
-    knex('users')
-        .select()  
-        .then(users => {
-            res.render("index", { users: users });
+    res.render("index")
+})
+
+app.get('/display', (req, res) => {
+    knex('volunteers')
+        .select()
+        .then(volunteers => {
+            res.render("display", { volunteers: volunteers });
         })
         .catch(err => {
             console.error("Error fetching users:", err);
             res.status(500).send("Error fetching data from the database");
         });
-      });
+});
 
-//   });
-// This starts the server to start listening to requests
+// Start the server
 app.listen(port, () => console.log(`Listening on port ${port}`));
