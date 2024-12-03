fa981139eaf1b25fa54874c170e3a830548314c0
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-03 09:20:53 -0700

    playing aroudn with certs

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -4,13 +4,9 @@
 # Place in .platform/hooks/postdeploy directory
 
 # Ensure Nginx is running before getting the certificate
-sudo systemctl start nginx
 
 # Certbot command to obtain and install the SSL certificate
-sudo certbot -n -d thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bcolton9@byu.edu
+sudo certbot -n -d "thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com" --nginx --agree-tos --email bcolton9@byu.edu
 
 
-certbot install --cert-name thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com
 
-# Restart Nginx to apply the SSL certificate
-sudo systemctl reload nginx
