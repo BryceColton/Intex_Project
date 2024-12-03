ed55c2e2b86bbebed7b0fa554250b88efe3cc479
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-03 10:32:34 -0700

    added domaind cert

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -24,3 +24,5 @@ nginx -t && service nginx restart
 
 # Deploy the certificate
 certbot install --cert-name thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com
+certbot install --cert-name group47intex.is404.net
+
