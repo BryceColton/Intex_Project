7f97ef1cb982a8994db46d22b92c2f14fa9c1aab
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-03 16:19:36 -0700

    fixing

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,26 +1,2 @@
-#!/bin/bash
 
-# Adjust Nginx settings
-NGINX_CONF="/etc/nginx/nginx.conf"
-
-# Add custom settings if not already present
-if ! grep -q "types_hash_max_size" $NGINX_CONF; then
-    echo "Adding types_hash_max_size to Nginx config..."
-    sed -i '/http {/a \    types_hash_max_size 2048;' $NGINX_CONF
-fi
-
-if ! grep -q "types_hash_bucket_size" $NGINX_CONF; then
-    echo "Adding types_hash_bucket_size to Nginx config..."
-    sed -i '/http {/a \    types_hash_bucket_size 128;' $NGINX_CONF
-fi
-
-if ! grep -q "server_names_hash_bucket_size" $NGINX_CONF; then
-    echo "Adding server_names_hash_bucket_size to Nginx config..."
-    sed -i '/http {/a \    server_names_hash_bucket_size 128;' $NGINX_CONF
-fi
-
-# Restart Nginx
-nginx -t && service nginx restart
-
-# Deploy the certificate
-certbot install --cert-name thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com
+sudo certbot -n -d thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bcolton9@byu.edu
