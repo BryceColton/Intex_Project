496f6baa27ec9c10ff05b1aeab5f68799ddc792c
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-03 10:04:51 -0700

    commands

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d ThisisTheApp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email cooperbt@byu.edu
+sudo certbot -n -d ThisisTheApp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bcolton9@byu.edu
 
 
 
