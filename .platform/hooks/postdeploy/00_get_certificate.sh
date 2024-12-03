61513a3c7d8084ce7de69d8c6c9afe5784f15970
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-03 09:52:35 -0700

    help

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-#!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-
-# Ensure Nginx is running before getting the certificate
-
-# Certbot command to obtain and install the SSL certificate
-sudo certbot -n -d http://thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email bcolton9@byu.edu
+sudo certbot -n -d ThisisTheApp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email cooperbt@byu.edu
 
 
 
