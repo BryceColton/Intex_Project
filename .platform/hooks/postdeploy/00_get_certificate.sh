03ec067283f65719e04245d2e360921558293547
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-02 20:24:54 -0700

    new certs

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
+sudo systemctl start nginx
+
+
 sudo certbot -n -d thisistheapp-env.eba-e4mhqj2t.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bcolton9@byu.edu
-    
+
+sudo systemctl reload nginx
