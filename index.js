29ffe507118e3448555a9086bf12cb24273eaa9c
cxxpertaylor <cooper.b.taylor@gmail.com>
2024-12-03 21:46:18 -0700

    akdsk

--- index.js
+++ index.js
@@ -236,6 +236,75 @@ app.post("/deleteEvent/:eventid", (req, res) => {
     });
 });
 
+// This route is for the admin to view declined events
+app.get("/adminDeclinedEvents", (req, res) => {
+  knex("events")
+    .select()
+    .where("status", "declined")
+    .then((declined_events) => res.render("declinedEvents", { declined_events }));
+});
+
+// This route is the get route for the admin to create their own events to show in the upcoming events table
+app.get("/adminAddEvent", (req, res) => {
+  res.render("adminAddEvent");
+});
+
+// This route is the post route for the admin to add an event
+app.post("/adminAddEvent", (req, res) => {
+  console.log(req.body);
+  const {
+    event_name,
+    organization,
+    organizer_first_name,
+    organizer_last_name,
+    org_phone,
+    org_email,
+    event_type,
+    event_address,
+    city,
+    state,
+    zip,
+    public,
+    guest_speaker,
+    venuedescription,
+    numexpected,
+    duration,
+    eventdatetime1,
+    eventdatetime2,
+  } = req.body;
+
+  knex("events")
+    .insert({
+      event_name,
+      organization,
+      organizer_first_name,
+      organizer_last_name,
+      org_phone,
+      org_email,
+      event_type,
+      event_address,
+      city,
+      state,
+      zip,
+      public: public === "on", // Convert checkbox to boolean
+      guest_speaker: guest_speaker === "on", // Convert checkbox to boolean
+      numexpected: parseInt(numexpected, 10), // Ensure it's stored as an integer
+      venuedescription,
+      duration: parseFloat(duration), // Ensure it's stored as a float
+      eventdatetime1: new Date(eventdatetime1), // Ensure it's stored as a valid Date
+      eventdatetime2: new Date(eventdatetime2), // Ensure it's stored as a valid Date
+      status: "approved",
+    })
+
+    .then(([eventid]) => {
+      res.redirect("/manageEvents");
+    })
+    .catch((err) => {
+      console.error("Database insert error:", err);
+      res.status(500).json({ message: "Error saving the event", error: err });
+    });
+});
+
 // ADMIN MANAGE VOLUNTEERS ROUTES ***********************************************************************************************************
 
 // This is the get method to render the manageVolunteers page and display data from the volunteers table
@@ -431,7 +500,7 @@ app.post("/submitVolunteerForm", (req, res) => {
   // Check if email already exists
   knex("volunteers")
     .where({ vol_email: vol_email })
-    .first()  // Returns the first match or null if no match
+    .first() // Returns the first match or null if no match
     .then((existingVolunteer) => {
       if (existingVolunteer) {
         return res.status(400).json({ message: "Email already exists." });
