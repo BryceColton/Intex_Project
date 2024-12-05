ff18e4f20e445f83cfddfc5be707267b6686d2a0
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2024-12-05 08:44:56 -0700

    envs

--- index.js
+++ index.js
@@ -36,18 +36,11 @@ const knex = require("knex")({
   connection: {
     host:
       process.env.RDS_HOSTNAME,
-    //   "awseb-e-auisjkadk8-stack-awsebrdsdatabase-evtdlb1elxoo.c98cyywwyjtd.us-east-1.rds.amazonaws.com",
-    user: process.env.RDS_USERNAME 
-    // || "ebroot"
-    ,
-    password: process.env.RDS_PASSWORD 
-    // || "Yodayoda663!"
-    ,
-    database: process.env.RDS_DB_NAME 
-    // || "ebdb"
-    ,
-    port: process.env.RDS_PORT || 5432,
-    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DB_NAME,
+      port: process.env.RDS_PORT || 5432,
+      ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
   },
 });
 // This is all of the information needed to access postgres
